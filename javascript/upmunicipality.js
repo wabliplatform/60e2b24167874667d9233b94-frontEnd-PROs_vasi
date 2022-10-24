6193ec3ca5ee14b2ea4c56b1509ef5231dc68b5a
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-10-24 23:07:27 +0300

    My commit message

--- javascript/upmunicipality.js
+++ javascript/upmunicipality.js
@@ -1,4 +1,4 @@
-let apiMunicipalityApi = new TempApi.MunicipalityApi();import TempApi from '../src/index';let apiCandidateApi = new TempApi.CandidateApi();
+let apiMunicipalityApi = new TempApi.MunicipalityApi();import TempApi from '../src/index';
  function calculateSize(img, maxWidth, maxHeight) {
       let width = img.width;
       let height = img.height;
@@ -67,22 +67,12 @@ document.getElementById('formFile').addEventListener("change", async(e) => {
           QUALITY
         );
       };});
-document.addEventListener('alignmcandidates', function(e) {
-  const advanceSelect = document.getElementById('i75jw');
-  const selectedElement = advanceSelect.getAttribute('selected-element');
-  if (!selectedElement) return;
-  [...advanceSelect.querySelectorAll("[annotationname]")].forEach(
-    optionElement => {
-      if (optionElement.value === selectedElement)
-        optionElement.setAttribute("selected", true);
-    }
-  );
-});document.getElementById('imuw9').onclick = (event) => {
+document.getElementById('ilubl').onclick = (event) => {
     event.preventDefault();
     let municipalityId = window.location.pathname.replace('/upmunicipality/','');let municipality = new TempApi.Municipality();municipality['mimage'] = {
         data: document.querySelector("[annotationname = 'mimage']").getAttribute("data-image-base64") !== null ? document.querySelector("[annotationname = 'mimage']").getAttribute("data-image-base64") : document.querySelector("[annotationname = 'mimage']").src,
         name: document.querySelector("[annotationname = 'mimage']").getAttribute("name")
-      };municipality['mname'] = document.querySelector("[annotationname = 'mname']").value;municipality['mcandidates'] = arrayiivls.map(item => item.value); let opts = {municipality};apiMunicipalityApi.updatemunicipality( municipalityId, opts, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); 
+      };municipality['mname'] = document.querySelector("[annotationname = 'mname']").value; let opts = {municipality};apiMunicipalityApi.updatemunicipality( municipalityId, opts, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); 
       if(response.body.query.mimage !== undefined){
 
         if(document.querySelector('[annotationname = mimage]').getAttribute('type') === 'file'){
@@ -93,7 +83,7 @@ document.addEventListener('alignmcandidates', function(e) {
         }
         document.querySelector('[annotationname = mimage]').name = response.body.query.mimage.name;
       }
-      document.querySelector('[annotationname = mname]').value = response.body.query.mname ;initializearrayiivls(response.body.query.mcandidates|| []) ; {   location.href= '/home' ;}}});};document.getElementById('iu6gg').onclick = (event) => {
+      document.querySelector('[annotationname = mname]').value = response.body.query.mname ;{   location.href= '/home' ;}}});};document.getElementById('ip5rc').onclick = (event) => {
     event.preventDefault();
     let municipalityId = window.location.pathname.replace('/upmunicipality/','');
       if(municipalityId === '/upmunicipality' || municipalityId === ''){
@@ -104,7 +94,7 @@ document.addEventListener('alignmcandidates', function(e) {
           if (
             document
               .getElementById(key)
-              .contains(document.getElementById("iu6gg")) === true &&
+              .contains(document.getElementById("ip5rc")) === true &&
               document.getElementById(key).contains(document.getElementById(parentId)) === false
           ) {
             municipalityId = value._id;
@@ -128,86 +118,4 @@ document.addEventListener('alignmcandidates', function(e) {
         }
         document.querySelector('[annotationname = mimage]').name = response.body.query.mimage.name ;
       }
-       } catch (e) { console.log(e) };try { 
-        document.querySelector('[annotationname = mcandidates]').setAttribute('selected-element',response.body.query.mcandidates.cname);document.dispatchEvent(new Event("alignmcandidates"));
-        const insideSubdocument = document.querySelector("[annotationname = 'mcandidates']");
-        if (insideSubdocument !==null) {
-          const tableData = response.body.query.mcandidates;
-    initializearrayiivls(tableData); 
- refreshULi9rqq();
-    const tableDataElement = insideSubdocument.querySelectorAll("[dataitem='true']");
-    tableData.forEach((data,index) => {
-      if(tableDataElement.length < index) {
-        return;
-      }
-       try {
-      const attributeSubdocumentElement = tableDataElement[
-        index
-      ].querySelector("[annotationname = 'cname']");
-      if (attributeSubdocumentElement !== null) {
-        attributeSubdocumentElement.textContent = tableData[tableData.length - index -1].cname;
-      }
-    }
-    catch(e) {console.log(e);};
-      
-      map.set(
-        tableDataElement[index].getAttribute("id"),
-        tableData[tableData.length - index -1]
-      );
-    
-    });
-    
-      [...tableDataElement].forEach((element, index) => {
-        if (index >= tableData.length) {
-          tableDataElement[index].style.display = "none";
-        }
-        else {
-          tableDataElement[index].style.display = "";
-        }
-      });
-    
-    
-        }
-      if(response.body.query.mcandidates._id){
-        map.set(
-          document.querySelector(
-            "[annotationname = 'mcandidates']"
-          ).getAttribute("id"),
-          response.body.query.mcandidates
-        );
-      }
-     } catch (e) { console.log(e) };window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));}});apiCandidateApi.getAllcandidate((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("iivls").querySelectorAll( "[dataitem='true']" )].filter(
-    (element, index, array) =>
-    !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
-  );const map = new Map();
-    if( data.length > subDataElements.length){
-      for(let i = 0; i <=  data.length - subDataElements.length; i++){
-        let parentNode = subDataElements[0].parentNode;
-        let child = parentNode.childNodes[0].cloneNode(true);
-        parentNode.appendChild(child);
-        subDataElements.push(child);
-      }
-    }
-    data.forEach((item,i) => {
-    if(subDataElements.length > i)
-      {
-        try { 
-      const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'cname']");
-      if(insideSubDataElement !== null){
-        insideSubDataElement.textContent = data[data.length -i -1].cname;
-        insideSubDataElement.value=data[data.length -i -1]._id;
-      }
-      else if(subDataElements[i].getAttribute('annotationname') === 'cname'){
-        subDataElements[i].textContent = data[data.length -i -1].cname;
-        subDataElements[i].value=data[data.length -i -1]._id;
-      }
-     } catch (e) { console.log(e) };
-        map.set(subDataElements[i].getAttribute('id'), data[data.length-i-1])
-        
-      }
-      document.dispatchEvent(new Event("alignmcandidates"))
-    });
-
-    window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));
-    
-    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
+       } catch (e) { console.log(e) };window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));}});};
