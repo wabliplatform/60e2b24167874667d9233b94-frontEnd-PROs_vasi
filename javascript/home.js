97f4866ea0af152f5715a417c7074c39536be8d7
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-10-24 22:00:57 +0300

    My commit message

--- javascript/home.js
+++ javascript/home.js
@@ -2,7 +2,9 @@ let apiMunicipalityApi = new TempApi.MunicipalityApi();import TempApi from '../s
     event.preventDefault();
     {   location.href= '/home' ;}};document.getElementById('if2fd').onclick = (event) => {
     event.preventDefault();
-    {   location.href= '/home' ;}};document.getElementById('i7e5h').onclick = (event) => {
+    {   location.href= '/allcandidates' ;}};document.getElementById('i4e9ar').onclick = (event) => {
+    event.preventDefault();
+    {   location.href= '/allcandidates' ;}};document.getElementById('i7e5h').onclick = (event) => {
     event.preventDefault();
     {   location.href= '/cmunicipality' ;}};document.getElementById('iwssa').onclick = (event) => {
     event.preventDefault();
