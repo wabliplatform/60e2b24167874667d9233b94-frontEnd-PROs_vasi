09eebbac5c5d4757d8c6e75b4a0d386c2ae95df3
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-11-02 12:37:28 +0200

    My commit message

--- javascript/candidate.js
+++ javascript/candidate.js
@@ -1,4 +1,4 @@
-let apiCandidateApi = new TempApi.CandidateApi();import TempApi from '../src/index';let apiConsultantApi = new TempApi.ConsultantApi();document.getElementById('ifuuql').onclick = (event) => {
+let apiCandidateApi = new TempApi.CandidateApi();import TempApi from '../src/index';document.getElementById('ifuuql').onclick = (event) => {
     event.preventDefault();
     {   location.href= '/home' ;}};document.getElementById('if2fd').onclick = (event) => {
     event.preventDefault();
@@ -98,19 +98,4 @@ let apiCandidateApi = new TempApi.CandidateApi();import TempApi from '../src/ind
           response.body.query.cconsultants
         );
       }
-     } catch (e) { console.log(e) };window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));}});apiConsultantApi.getAllconsultant((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("i5xbl").querySelectorAll( "[dataitem='true']" )].filter(
-    (element, index, array) =>
-    !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
-  );const map = new Map();  data.forEach((item,i) => {
-    if(subDataElements.length > i)
-      {
-        console.log('There are no inside data elements');
-        map.set(subDataElements[i].getAttribute('id'), data[data.length-i-1])
-        
-      }
-      
-    });
-
-    window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));
-    
-    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
+     } catch (e) { console.log(e) };window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));}});};
