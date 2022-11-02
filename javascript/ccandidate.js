d901b56bc55df4f331ca1432e83dde85e01a4287
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-11-02 12:25:08 +0200

    My commit message

--- javascript/ccandidate.js
+++ javascript/ccandidate.js
@@ -79,6 +79,41 @@ document.getElementById('formFile').addEventListener("change", async(e) => {
           QUALITY
         );
       };});
+let arrayi6k9i = [];
+document.getElementById("izi8a").onclick = event => {
+  event.preventDefault();
+  const select = document.getElementById("iso6o")
+  arrayi6k9i.push({
+      value: select.value,
+      liValue: select.selectedOptions[0].textContent
+  });
+  select.value = "";
+  select.selectedIndex = 0;
+  refreshULixaze();
+};
+
+function refreshULixaze() {
+let e=``;
+for (let y=0; y<arrayi6k9i.length; y++)
+ {
+   e += `<li index='${y}' arrayvalue='${arrayi6k9i[y].value}'><p style="display: inline-block">${arrayi6k9i[y].liValue}</p><button class="btn pointer bi bi-trash delete-btn" style="display: inline-block;float: right;background-color: red;color: white;" index='${y}'>&nbsp;Delete</button></li>`;
+ }
+document.getElementById("ixaze").innerHTML = e;
+}
+
+document.getElementById("ixaze").addEventListener("click", event => {
+  event.preventDefault();
+  arrayi6k9i = arrayi6k9i.filter(
+    (item, index) => +event.target.getAttribute("index") !== index
+  );
+  refreshULixaze();
+});
+function initializearrayi6k9i(data) {
+  arrayi6k9i = data.map(item => ({
+    value: item._id,
+    liValue: item['consname']
+  }));
+}
 document.addEventListener('aligncconsultants', function(e) {
   const advanceSelect = document.getElementById('iso6o');
   const selectedElement = advanceSelect.getAttribute('selected-element');
@@ -95,7 +130,7 @@ document.addEventListener('aligncconsultants', function(e) {
         data: document.querySelector("[annotationname = 'cimage']").getAttribute("data-image-base64") !== null ? document.querySelector("[annotationname = 'cimage']").getAttribute("data-image-base64") : document.querySelector("[annotationname = 'cimage']").src,
         name: document.querySelector("[annotationname = 'cimage']").getAttribute("name")
       };
-      candidate['cname'] = document.querySelector("[annotationname = 'cname']").value;candidate['cbio'] = document.querySelector("[annotationname = 'cbio']").value;candidate["cconsultants"] = [...document.querySelector("[annotationname = 'cconsultants']").querySelectorAll("[arrayvalue]")].map(li=> li.getAttribute('arrayvalue'));apiCandidateApi.createcandidate( candidate, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); {   location.href= '/home' ;}}});};window.onload = () => {apiConsultantApi.getAllconsultant((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("i6k9i").querySelectorAll( "[dataitem='true']" )].filter(
+      candidate['cname'] = document.querySelector("[annotationname = 'cname']").value;candidate['cbio'] = document.querySelector("[annotationname = 'cbio']").value;candidate["cconsultants"] = [...document.querySelector("[annotationname = 'cconsultants']").querySelectorAll("[arrayvalue]")].map(li=> li.getAttribute('arrayvalue'));apiCandidateApi.createcandidate( candidate, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); {   location.href= '/home' ;}}});};window.onload = () => {apiConsultantApi.getAllconsultant((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("ifj1f").querySelectorAll( "[dataitem='true']" )].filter(
     (element, index, array) =>
     !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
   );const map = new Map();
