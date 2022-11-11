371a1d5b5da6a5ff56658cf361be768c477eabcb
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-11-11 14:01:05 +0200

    My commit message

--- javascript/Page10.js
+++ javascript/Page10.js
@@ -1,4 +1,4 @@
-document.getElementById('ifuuql').onclick = (event) => {
+let apiMunicipalityApi = new TempApi.MunicipalityApi();import TempApi from '../src/index';document.getElementById('ifuuql').onclick = (event) => {
     event.preventDefault();
     {   location.href= '/' ;}};document.getElementById('if2fd').onclick = (event) => {
     event.preventDefault();
@@ -464,4 +464,39 @@ document.getElementById('ifuuql').onclick = (event) => {
           parentId = key;
         }
       });
-     location.href= '/upmunicipality/' + transitionId;}};window.onload = () => {};
+     location.href= '/upmunicipality/' + transitionId;}};window.onload = () => {apiMunicipalityApi.getAllmunicipality((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("iof5e").querySelectorAll( "[dataitem='true']" )].filter(
+    (element, index, array) =>
+    !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
+  );const map = new Map();  data.forEach((item,i) => {
+    if(subDataElements.length > i)
+      {
+        try { 
+const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'mimage']");
+if(insideSubDataElement !== null && data[data.length -i -1].mimage !== undefined){
+  insideSubDataElement.src = data[data.length -i -1].mimage.data;
+  insideSubDataElement.name = data[data.length -i -1].mimage.name;
+}
+else if(subDataElements[i].getAttribute('annotationname') === 'mimage' && data[data.length -i -1].mimage !== undefined){
+  subDataElements[i].src = data[data.length -i -1].mimage.data;
+  subDataElements[i].name = data[data.length -i -1].mimage.name;
+}
+ } catch (e) { console.log(e) };try { 
+      const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'mname']");
+      if(insideSubDataElement !== null){
+        insideSubDataElement.textContent = data[data.length -i -1].mname;
+        
+      }
+      else if(subDataElements[i].getAttribute('annotationname') === 'mname'){
+        subDataElements[i].textContent = data[data.length -i -1].mname;
+        
+      }
+     } catch (e) { console.log(e) };
+        map.set(subDataElements[i].getAttribute('id'), data[data.length-i-1])
+        
+      }
+      
+    });
+
+    window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));
+    
+    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
